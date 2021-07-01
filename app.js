897a1e1a1481623a844689f353b27c5e7ef9b890
Sarakafaween <>
2021-07-01 11:46:12 +0000

    add

--- app.js
+++ app.js
@@ -4,8 +4,33 @@
  console.log(name)
  alert("wlecome to my website " + name + " :)" )
 
- var subject = prompt("which subject want ?")
+ var subject = prompt("what is your subject want? (math , physics)")
 
+while(subject != "math" && favAnime != "physics"){
+  subject = prompt("what is your subject want? (math , physics)")
+}
+
+
+if( subject == "math" ){
+var times= prompt("how many time you wish to see it on my page ")
+
+for(var i = 0 ; i < times; i++) {
+document.write("<div>" + "<h3>" + subject +"</h3>" + "<img src='https://th.bing.com/th/id/R0ab3cc9933bae5bba3af8cf391331c53?rik=tD16PJ8%2flhUUyg&riu=http%3a%2f%2fwww.niutoday.info%2fwp-content%2fuploads%2f2016%2f02%2fmath.jpg&ehk=vYIr3iPobA7dMHZ5Be3j1rWxFcdupxIY8Fn28sTZFCA%3d&risl=&pid=ImgRaw' alt='math'>" + "</div>")
+}
+
+}
+else if (subject == "physics"){
+  var times= prompt("how many time you wish to see it on my page ")
+while(times > 10){
+  times = prompt("please enter less than 10 time to see you subject")
+}
+for(var i = 0 ; i < times; i++) {
+document.write("<div>" + "<h3>" + subject +"</h3>" + "<img src='https://th.bing.com/th/id/OIP.XuHaqlIzTRPwW37MtcjouQAAAA?w=252&h=189&c=7&o=5&pid=1.7' alt='physics'>" + "</div>")
+}
+}
+else{
+  alert("we don't support this subject")
+}
  console.log(subject)
 
  if (subject != null)
