23d319eb869a55fcdfa226361d9c805370d8c06d
Sarakafaween <>
2021-07-04 07:10:59 +0000

    add

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-
-
  var name = prompt("what is you name?")
  console.log(name)
  alert("wlecome to my website " + name + " :)" )
 
  var subject = prompt("what is your subject want? (math , physics)")
 
-while(subject != "math" && favAnime != "physics"){
+while(subject != "null"){
   subject = prompt("what is your subject want? (math , physics)")
+  
 }
 
 
 if( subject == "math" ){
 var times= prompt("how many time you wish to see it on my page ")
-
+while(times > 10){
+  times = prompt("please enter less than 10 time to see you subject")
+}
 for(var i = 0 ; i < times; i++) {
 document.write("<div>" + "<h3>" + subject +"</h3>" + "<img src='https://th.bing.com/th/id/R0ab3cc9933bae5bba3af8cf391331c53?rik=tD16PJ8%2flhUUyg&riu=http%3a%2f%2fwww.niutoday.info%2fwp-content%2fuploads%2f2016%2f02%2fmath.jpg&ehk=vYIr3iPobA7dMHZ5Be3j1rWxFcdupxIY8Fn28sTZFCA%3d&risl=&pid=ImgRaw' alt='math'>" + "</div>")
 }
